372828c7b5ff6e73757a2bd11ac30a1dbc812930
pjoe <pelle.johnsen@gmail.com>
2022-07-14 10:08:47 +0200

    2 files modified, 1 added

---
+++ src/parser/token.ts
@@ -0,0 +1,64 @@
+export type TokenType =
+  // assign
+  | 'Assign'
+  | 'AddAssign'
+  | 'SubAssign'
+  | 'MulAssign'
+  | 'DivAssign'
+  // binary ops
+  | 'Plus'
+  | 'Dash'
+  | 'Star'
+  | 'Slash'
+  | 'Power'
+  // conditons
+  | 'Equal'
+  | 'GreaterEqual'
+  | 'LessEqual'
+  // unary ops
+  | 'Inc'
+  | 'Dec'
+  // paren
+  | 'LParen'
+  | 'RParen'
+  | 'LBracket'
+  | 'RBracket'
+  | 'LBrace'
+  | 'RBrace'
+  | 'LAngle'
+  | 'RAngle'
+  // consts
+  | 'IntConst'
+  | 'FloatConst'
+  | 'BoolConst'
+  // identifiers
+  | 'Ident'
+  // keywords
+  | 'If'
+  | 'Else'
+  | 'Break'
+  | 'Continue'
+  | 'While'
+  | 'Let'
+  | 'Return'
+  // others
+  | 'Comma'
+  | 'Colon'
+  | 'Semicolon'
+  | 'Dot'
+  | 'Quote'
+  | 'Arrow'
+  | 'EOF'
+  | 'Unknown';
+
+export type TokenValue = string | number | boolean;
+
+export interface Token {
+  type: TokenType;
+  value?: TokenValue;
+  pos: {
+    line: number;
+    start: number;
+    end: number;
+  };
+}
