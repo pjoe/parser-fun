f59d56a44589c454e04d3592215af2cf7d8f601e
pjoe <pelle.johnsen@gmail.com>
2022-07-18 19:27:37 +0200

    Complete func-lexer

--- src/parser/func-lexer.ts
+++ src/parser/func-lexer.ts
@@ -15,35 +15,60 @@ import { Token } from './token';
 
 export interface FuncLexContext extends LexContext {
   lookahead: Token[];
+  funcParen: Token | null;
 }
 
 export const makeContext = (getChar: GetCharFn): FuncLexContext => ({
   ..._makeContext(getChar),
   lookahead: [],
+  funcParen: null,
 });
 
 export const next = (ctx: FuncLexContext): Token => {
   let t: Token;
+  if (ctx.funcParen) {
+    t = ctx.funcParen;
+    ctx.funcParen = null;
+    return t;
+  }
   if (ctx.lookahead.length > 0) t = ctx.lookahead.shift()!;
   else t = _next(ctx);
   if (t.type === 'LParen') {
-    const p = ctx.parenCount;
+    let parenCount = 1;
+    let bufPos = 0;
+    const bufLen = ctx.lookahead.length;
+    let rParenMatch = false;
     while (true) {
       let t2: Token;
-      if (ctx.lookahead.length > 0) t2 = ctx.lookahead.shift()!;
-      else t2 = _next(ctx);
+      if (bufPos < bufLen) {
+        t2 = ctx.lookahead[bufPos];
+        ++bufPos;
+      } else {
+        t2 = _next(ctx);
+        ctx.lookahead.push(t2);
+      }
       if (t2.type === 'EOF') break;
-      ctx.lookahead.push(t2);
-      if (t2.type === 'RParen' && ctx.parenCount === p - 1) {
-        ctx.lookahead.unshift(t);
-        return {
-          type: 'Func',
-          pos: {
-            line: t.pos.line,
-            start: t.pos.start,
-            end: t.pos.end,
-          },
-        };
+      if (rParenMatch) {
+        if (t2.type === 'Arrow' && rParenMatch) {
+          ctx.funcParen = t;
+          return {
+            type: 'Func',
+            pos: {
+              line: t.pos.line,
+              start: t.pos.start,
+              end: t.pos.end,
+            },
+          };
+        } else break;
+      }
+      rParenMatch = false;
+      if (t2.type === 'LParen') {
+        ++parenCount;
+      } else if (t2.type === 'RParen') {
+        --parenCount;
+        if (parenCount === 0) {
+          rParenMatch = true;
+        }
       }
     }
   }
